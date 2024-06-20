c6808b4bbb959ed1af80ce8931dbd47dcc68abe9
elorisraeli <elorisraeli@gmail.com>
2024-06-20 14:43:29 +0300

    remove roll from log and video + fix warnings

--- src/detector.py
+++ src/detector.py
@@ -12,7 +12,7 @@ ARUCO_MARKER_SIZE = 0.05  # Size of the Aruco marker in meters
 CAMERA_MATRIX = np.array([[921.170702, 0.000000, 459.904354],
                           [0.000000, 919.018377, 351.238301],
                           [0.000000, 0.000000, 1.000000]])
-DISTORTION_COEFFS = np.array([-0.033458, 0.105152, 0.001256, -0.006647, 0.000000])
+DISTORTION_COEFFICIENTS = np.array([-0.033458, 0.105152, 0.001256, -0.006647, 0.000000])
 
 
 def setup_paths():
@@ -23,25 +23,25 @@ def setup_paths():
         tuple: Paths for input video, output video, CSV file, and log file.
     """
     base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    video_path = os.path.join(base_dir, "TestVideos", "classVideo.mp4")
-    output_video_path = os.path.join(base_dir, "Output", "output_video.mp4")
-    csv_path = os.path.join(base_dir, "Output", "output_data.csv")
-    log_path = os.path.join(base_dir, "Output", "app.log")
-    return video_path, output_video_path, csv_path, log_path
+    video_path_to_modify = os.path.join(base_dir, "TestVideos", "classVideo.mp4")
+    output_video_path_after_modification = os.path.join(base_dir, "Output", "output_video.mp4")
+    csv_path_for_data = os.path.join(base_dir, "Output", "output_data.csv")
+    log_path_from_logger = os.path.join(base_dir, "Output", "app.log")
+    return video_path_to_modify, output_video_path_after_modification, csv_path_for_data, log_path_from_logger
 
 
 # Get paths and setup logging
-video_path, output_video_path, csv_path, log_path = setup_paths()
-logging.basicConfig(level=logging.DEBUG, filename=log_path, filemode='w',
+_, _, _, log_path_for_config = setup_paths()  # needs here only for the logging.basicConfig
+logging.basicConfig(level=logging.DEBUG, filename=log_path_for_config, filemode='w',
                     format='%(name)s - %(levelname)s - %(message)s')
 
 
-def initialize_video_capture(video_path):
+def initialize_video_capture(input_video_path):
     """
     Initializes the video capture object.
 
     Args:
-        video_path (str): Path to the input video file.
+        input_video_path (str): Path to the input video file.
 
     Returns:
         cv2.VideoCapture: Video capture object.
@@ -49,7 +49,7 @@ def initialize_video_capture(video_path):
     Raises:
         IOError: If the video file cannot be opened.
     """
-    cap = cv2.VideoCapture(video_path)
+    cap = cv2.VideoCapture(input_video_path)
     if not cap.isOpened():
         logging.error("Cannot open video file")
         raise IOError("Cannot open video file")
@@ -74,7 +74,7 @@ def setup_video_writer(cap, output_video_path):
     return out
 
 
-def draw_annotations(frame, corners, ids, distances, yaw_angles, roll_angles):
+def draw_annotations(frame, corners, ids, distances, yaw_angles):
     """
     Draws annotations on the frame, including Aruco ID, distance, yaw angle, and roll angle.
 
@@ -84,7 +84,6 @@ def draw_annotations(frame, corners, ids, distances, yaw_angles, roll_angles):
         ids (list): List of IDs of detected Aruco markers.
         distances (list): List of distances to the detected Aruco markers.
         yaw_angles (list): List of yaw angles of the detected Aruco markers.
-        roll_angles (list): List of roll angles of the detected Aruco markers.
     """
     for i, corner in enumerate(corners):
         pts = corner.reshape((4, 2)).astype(np.int32)
@@ -100,9 +99,6 @@ def draw_annotations(frame, corners, ids, distances, yaw_angles, roll_angles):
             text_position = (pts[3][0][0], pts[3][0][1] + 90)
             cv2.putText(frame, f'Yaw: {yaw_angles[i]:.2f} deg', text_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
                         2)
-            text_position = (pts[3][0][0], pts[3][0][1] + 120)
-            cv2.putText(frame, f'Roll: {roll_angles[i]:.2f} deg', text_position, cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        (0, 255, 0), 2)
 
 
 def calculate_pose_and_distance(corners):
@@ -115,20 +111,21 @@ def calculate_pose_and_distance(corners):
     Returns:
         tuple: Distance, yaw angle, pitch angle, roll angle, translation vector, and rotation vector.
     """
-    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, ARUCO_MARKER_SIZE, CAMERA_MATRIX, DISTORTION_COEFFS)
-    distance = np.linalg.norm(tvec)
-    yaw_angle = np.arctan2(tvec[0][0][0], tvec[0][0][2])
+    rotation_vec, translation_vec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, ARUCO_MARKER_SIZE, CAMERA_MATRIX,
+                                                                           DISTORTION_COEFFICIENTS)
+    distance = np.linalg.norm(translation_vec)
+    yaw_angle = np.arctan2(translation_vec[0][0][0], translation_vec[0][0][2])
     yaw_angle_degrees = np.degrees(yaw_angle)
     yaw_angle_degrees = (yaw_angle_degrees + 180) % 360 - 180  # Normalize to -180 to 180 degrees
 
     # Convert rotation vector to rotation matrix
-    R, _ = cv2.Rodrigues(rvec[0][0])
+    matrix_rotation_vec, _ = cv2.Rodrigues(rotation_vec[0][0])
 
     # Calculate pitch, yaw, and roll from the rotation matrix
-    pitch_angle = np.degrees(np.arctan2(R[1, 0], R[0, 0]))
-    roll_angle = np.degrees(np.arctan2(R[2, 1], R[2, 2]))
+    pitch_angle = np.degrees(np.arctan2(matrix_rotation_vec[1, 0], matrix_rotation_vec[0, 0]))
+    roll_angle = np.degrees(np.arctan2(matrix_rotation_vec[2, 1], matrix_rotation_vec[2, 2]))
 
-    return distance, yaw_angle_degrees, pitch_angle, roll_angle, tvec, rvec
+    return distance, yaw_angle_degrees, pitch_angle, roll_angle, translation_vec, rotation_vec
 
 
 def process_video(cap, out, csv_writer):
@@ -142,7 +139,8 @@ def process_video(cap, out, csv_writer):
     """
     logging.info("Processing video.")
     aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
-    parameters = aruco.DetectorParameters_create()
+    # parameters = aruco.DetectorParameters() # For Linux users
+    parameters = aruco.DetectorParameters_create()  # For Windows users
     frame_id = 0
 
     while True:
@@ -159,16 +157,19 @@ def process_video(cap, out, csv_writer):
             yaw_angles = []
             roll_angles = []
             for i, aruco_id in enumerate(ids):
-                distance, yaw_angle, pitch_angle, roll_angle, tvec, rvec = calculate_pose_and_distance(corners[i])
+                distance, yaw_angle, pitch_angle, roll_angle, \
+                    translation_vector, rotation_vector = calculate_pose_and_distance(corners[i])
                 distances.append(distance)
                 yaw_angles.append(yaw_angle)
                 roll_angles.append(roll_angle)
-                csv_writer.writerow([frame_id, aruco_id[0],
-                                     f"[{corners[i][0][0]}, {corners[i][0][1]}, {corners[i][0][2]}, {corners[i][0][3]}]",
-                                     f"[{distance}, {yaw_angle}, {pitch_angle}, {roll_angle}]"])
+                csv_writer.writerow(
+                    [frame_id, aruco_id[0],
+                     f"[{corners[i][0][0]}, {corners[i][0][1]}, {corners[i][0][2]}, {corners[i][0][3]}]",
+                     f"[{distance}, {yaw_angle}, {pitch_angle}]"])
                 logging.info(
-                    f"Frame {frame_id}: Aruco ID {aruco_id[0]} detected with distance {distance}, yaw angle {yaw_angle}, and roll angle {roll_angle}.")
-            draw_annotations(frame, corners, ids, distances, yaw_angles, roll_angles)
+                    f"Frame {frame_id}: Aruco ID {aruco_id[0]} detected with distance {distance},"
+                    f" yaw angle {yaw_angle}, pitch angle {pitch_angle}, roll angle {roll_angle}.")
+            draw_annotations(frame, corners, ids, distances, yaw_angles)
 
         out.write(frame)
         frame_id += 1
@@ -188,13 +189,13 @@ def release_resources(cap, out):
     logging.info("Resources released.")
 
 
-def show_output_video(output_video_path):
+def show_output_video(output_video_path_result):
     """
     Displays the output video in real-time with controls for pausing, resuming, and stepping through frames.
     Args:
-        output_video_path (str): Path to the output video file.
+        output_video_path_result (str): Path to the output video file.
     """
-    cap = cv2.VideoCapture(output_video_path)
+    cap = cv2.VideoCapture(output_video_path_result)
     if not cap.isOpened():
         logging.error("Error: Could not open the output video.")
         return
@@ -226,11 +227,14 @@ def show_output_video(output_video_path):
 
         key = cv2.waitKey(delay) & 0xFF
 
-        if key == ord('q') or key == ord('e'):  # Press 'q' or 'e' to quit the video display early
+        if key == ord('q') or key == ord('e') or key == 'q' or key == 'e':  # Press 'q' or 'e'
+            # to quit the video display early
             break
-        elif key == ord('p') or key == ' ':  # Press 'p' or 'space' to pause/resume the video display
+        elif key == ord('p') or key == ord(' ') or key == ' ' or key == 'p':  # Press 'p' or 'space'
+            # to pause/resume the video display
             paused = not paused
-        elif key == ord('d') or key == ord('D'):  # Press 'd' to step forward one frame when paused
+        elif key == ord('d') or key == ord('D') or key == 'd' or key == 'D':  # Press 'd'
+            # to step forward one frame when paused
             paused = True
             ret, frame = cap.read()
             if not ret:
